7ce80d8b74cb562586f338332a46c6564dd74c11
gamsus <92788796+gamsus@users.noreply.github.com>
2023-01-13 07:11:05 +0100

    pierwszy sprint do pokazania przed swietami. TODO: server i klient, dokonczyc tryb i stworzyc kolejne w nastepnym sprincie.

--- src/main/java/com/example/tp4/HelloApplication.java
+++ src/main/java/com/example/tp4/HelloApplication.java
@@ -1,17 +1,33 @@
 package com.example.tp4;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) {
+
+        TilePane pane = new TilePane();
+        Button button1 = new Button("English version");
+        pane.getChildren().add(button1);
+        Scene scene = new Scene(pane, 800, 600);
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                Game game = new Game();
+                Scene scene1 = new Scene(game, 800, 600);
+                stage.setScene(scene1);
+            }
+        });
+
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
